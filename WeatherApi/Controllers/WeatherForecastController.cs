84b6cd51f531009df21dc1eda564e0fb95ede4e5
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-02-04 15:18:54 -0300

    ReNamed, use Repository

---
+++ WeatherApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApi.Interfaces;
+using WeatherApi.Repository;
+
+namespace WeatherApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController(
+        [FromKeyedServices("ApiRepository")]
+        IRepositoryWeatherForecast apiRepository
+    ) : Controller
+    {
+        private readonly IRepositoryWeatherForecast _apiRepository = apiRepository;
+
+        [HttpGet("GetWeatherForecast")]
+        public async Task<IActionResult> GetWeatherForecastData()
+        {
+            try
+            {
+                var result = await _apiRepository.GetWeatherForecastData();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            
+        }
+    }
+}
