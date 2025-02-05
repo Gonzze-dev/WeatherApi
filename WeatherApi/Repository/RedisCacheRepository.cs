3f543ee55b62f7ba9d33328a98ae0df87eb5d3dc
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-02-05 19:02:50 -0300

    trry get data

--- WeatherApi/Repository/RedisCacheRepository.cs
+++ WeatherApi/Repository/RedisCacheRepository.cs
@@ -1,12 +1,37 @@
-﻿using WeatherApi.Interfaces;
+﻿using System.Text.Json;
+using RedisDB;
+using StackExchange.Redis;
+using WeatherApi.Interfaces;
+using WeatherApi.Repository.utils;
 
 namespace WeatherApi.Repository
 {
-    public class RedisCacheRepository : IRepositoryWeatherForecast
+    public class RedisCacheRepository(
+        RedisDBConnection redisDB
+        ) : IRepositoryWeatherForecast
     {
+        readonly IDatabase _DBConnection = redisDB.Connection.GetDatabase();
+
         public async Task<object> GetWeatherForecastData()
         {
-            throw new NotImplementedException();
+            var redisResult = await _DBConnection.StringGetAsync("PrimeraData");
+
+            if (redisResult.IsNullOrEmpty)
+                throw new Exception("Error to get data from Redis DB");
+
+            var redisData = redisResult.HasValue ? redisResult.ToString() : string.Empty;
+
+            var isJsonValid = Utils.ValidatedJson(redisData);
+
+            if (!isJsonValid)
+                throw new Exception("Error invalid fromat of json");
+
+            var result = JsonSerializer.Deserialize<object>(redisData);
+
+            if (result == null)
+                throw new Exception("Error: deserialization returned null");
+
+            return result;
         }
 
         public async Task Save()
---
+++ WeatherApi/Repository/utils/Utils.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using System.Text.Json;
+
+namespace WeatherApi.Repository.utils
+{
+    public static class Utils
+    {
+        public static bool ValidatedJson(string json)
+        {
+            try
+            {
+                using JsonDocument doc = JsonDocument.Parse(json);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
