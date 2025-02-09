17527396fc80a62f0367ec8eb1cbdd130852ef84
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-02-08 22:56:40 -0300

    -DEL Errors list -Fire and Forget for Save data in Redis

--- WeatherApi/Services/WeatherForecastService.cs
+++ WeatherApi/Services/WeatherForecastService.cs
@@ -4,7 +4,7 @@ using WeatherApi.Helpers;
 using WeatherApi.Interfaces;
 using WeatherApi.Repository;
 
-namespace WeatherApi.Service
+namespace WeatherApi.Services
 {
     public class WeatherForecastService
     (
@@ -14,12 +14,10 @@ namespace WeatherApi.Service
         WeatherForecastRepositoryHelper weatherForecastRepositoryHelper
     )
     {
-        public readonly List<string> Errors = [];
 
         readonly IRepositoryWeatherForecast<HttpResponseMessage?> _apiRepository = apiRepository;
         readonly RedisCacheRepository _redisCacheRepository = redisCacheRepository;
         readonly WeatherForecastRepositoryHelper _weatherForecastRepositoryHelper = weatherForecastRepositoryHelper;
-
 
         public async Task<object?> GetWeatherForecastData()
         {
@@ -30,34 +28,17 @@ namespace WeatherApi.Service
             {
                 result = _weatherForecastRepositoryHelper.ResponseHelper(resultRedis);
 
-                if (_weatherForecastRepositoryHelper.Validate())
-                {
-                    Errors.AddRange(_weatherForecastRepositoryHelper.Errors);
-                    return null;
-                }
 
                 return result;
-
             }
 
             HttpResponseMessage? resultApi = await _apiRepository.GetWeatherForecastData();
 
             result = await _weatherForecastRepositoryHelper.ResponseHelper(resultApi);
 
-            if (_weatherForecastRepositoryHelper.Validate())
-            {
-                Errors.AddRange(_weatherForecastRepositoryHelper.Errors);
-                return null;
-            }
-
-            _redisCacheRepository.Save(result);
+            _ = _redisCacheRepository.Save(result); //fire and forget!
 
             return result;
-        }
-
-        public bool Validate()
-        {
-            return Errors.Count > 0;
         }
     }
 }
